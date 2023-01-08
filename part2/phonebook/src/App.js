ef8cd8eb0a59d5d3c847586c4914c5d1a6b51f3b
Nicolás Olivera <nnicolivera@hotmail.com>
2023-01-08 00:14:09 -0300

    2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '35184423'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -41,7 +42,9 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>filter shown with <input onChange={(e) => setSearch(e.target.value)}></input></div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input
@@ -56,8 +59,12 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      <ul>{persons.map(x => (
-        <li key={x.name}>{x.name} {x.number}</li>
+      <ul>{persons.filter((item) => {
+        return search.toLocaleLowerCase() === ''
+          ? item
+          : item.name.toLocaleLowerCase().includes(search)
+      }).map(item => (
+        <li key={item.name}>{item.name} {item.number}</li>
       ))}</ul>
     </div>
   )
